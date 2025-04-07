046a9ca2b476e05fb9ea0aac8621b436e739c84f
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2025-04-07 15:29:28 +0200

    changed colors
    next todo:
    rewrite it asynchronous

--- GUI.py
+++ GUI.py
@@ -6,18 +6,22 @@ import requests
 from io import BytesIO
 import os
 
+CLR = 'lightyellow'
+
 pokemon_data = GetData()
 
 class GraphicInterface:
     def __init__(self):
         self.window = Tk()
+        self.window.title('Pokedex')
+        self.window.config(bg=CLR)
         self.window.minsize(500, 500)
-        self.canvas = Canvas(self.window, width=500, height=500, bg='lightyellow')
+        self.canvas = Canvas(self.window, width=500, height=500, bg=CLR)
         self.images = []
         self.column = 0
         self.row = 0
         self.canvas.grid(row=0, column=0)
-        self.inner_frame = Frame(self.canvas)
+        self.inner_frame = Frame(self.canvas, bg=CLR)
         self.canvas.create_window((0, 0), window=self.inner_frame, anchor='nw')
         self.scrollbar = Scrollbar(self.window, orient=VERTICAL, command=self.canvas.yview)
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
@@ -27,7 +31,7 @@ class GraphicInterface:
 
     def add_pokemon(self, pokemon_id):
         pokemon_list, sprite_list = pokemon_data.fetch_data()
-        for i in range(0, 151):
+        for i in range(0, 151):#number of pokemons
             #Getting list of pokemons and their sprites in a loop
             pokemon = pb.pokemon(pokemon_list[i])
             sprite = pb.SpriteResource('pokemon', pokemon.id)
@@ -44,14 +48,14 @@ class GraphicInterface:
             image = Image.open(f'sprites/{pokemon.id}.png')
             photo = ImageTk.PhotoImage(image)
             #frame with photo
-            frame = Frame(self.inner_frame)
-            pokemon_img = Label(frame, image=photo)
+            frame = Frame(self.inner_frame, bg=CLR)
+            pokemon_img = Label(frame, image=photo, bg=CLR)
             pokemon_img.grid(row=0, column=self.column)
             #pokemon name
-            pokemon_name = Label(frame, text=pokemon_list[i].capitalize())
+            pokemon_name = Label(frame, text=pokemon_list[i].capitalize(), bg=CLR)
             pokemon_name.grid(row=1, column=self.column)
             #pokemon type
-            pokemon_type = Label(frame, text=pokemon.types[0].type.name)
+            pokemon_type = Label(frame, text=pokemon.types[0].type.name, bg=CLR)
             pokemon_type.grid(row=2, column=self.column)
             frame.grid(row = self.row, column = self.column, pady=10, padx=10)
             self.column += 1
