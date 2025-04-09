1e2aa00b84aef8046d73d30ce93725c3225e2f3e
Hubert Mikielewicz <152910024+Hiobos@users.noreply.github.com>
2025-04-09 10:50:18 +0200

    Asynchronous loading
    now it's possible to scroll while app loads data, also changed API request params location to the file with main code logic in order to be able to manipulate it if user chooses to

--- GUI.py
+++ GUI.py
@@ -11,69 +11,78 @@ pokemon_data = GetData()
 
 class GraphicInterface:
     def __init__(self):
+        #main window
         self.window = Tk()
         self.window.title('Pokedex')
         self.window.config(bg=CLR)
         self.window.minsize(500, 500)
+
+        #Setting range of Pokemon from API and fetching data
+        self.offset, self.limit = 0, 151
+        param = {
+            'offset': self.offset,
+            'limit': self.limit
+        }
+        self.pokemon_list = pokemon_data.fetch_data(param)
+
+        #defining empty variables
+        self.images, self.column, self.row = [], 0, 0
+        #canvas, frame, scrollbar
         self.canvas = Canvas(self.window, width=500, height=500, bg=CLR)
-        self.images = []
-        self.column = 0
-        self.row = 0
         self.canvas.grid(row=0, column=0)
         self.inner_frame = Frame(self.canvas, bg=CLR)
         self.canvas.create_window((0, 0), window=self.inner_frame, anchor='nw')
         self.scrollbar = Scrollbar(self.window, orient=VERTICAL, command=self.canvas.yview)
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
         self.scrollbar.grid(row=0, column=1, sticky='ns')
-        self.add_pokemon(1)
+        self.add_pokemon(0)
         self.window.mainloop()
 
-    def add_pokemon(self, pokemon_id):
-        pokemon_list, sprite_list = pokemon_data.fetch_data()
-        for i in range(0, 151):#number of pokemons
-            #Getting list of pokemons and their sprites in a loop
-            pokemon = pb.pokemon(pokemon_list[i])
-            sprite = pb.SpriteResource('pokemon', pokemon.id)
-            sprite_url = sprite.url
-            #getting photo to PhotoImage
-            if not os.path.exists('sprites'):
-                os.mkdir('sprites')
+    def add_pokemon(self, i=0):
 
-            if not os.path.exists(f'sprites/{pokemon.id}.png'):
-                response = requests.get(sprite_url)
-                with open(f"sprites/{pokemon.id}.png", 'wb') as file:
-                    file.write(response.content)
+        if i >= len(self.pokemon_list):
+            return 0
+        print(f"i: {i}, len(poke_list): {len(self.pokemon_list)}")
 
-            image = Image.open(f'sprites/{pokemon.id}.png')
-            photo = ImageTk.PhotoImage(image)
-            #frame with photo
-            frame = Frame(self.inner_frame, bg=CLR)
-            pokemon_img = Label(frame, image=photo, bg=CLR)
-            pokemon_img.grid(row=0, column=self.column)
-            #pokemon name
-            pokemon_name = Label(frame, text=pokemon_list[i].capitalize(), bg=CLR)
-            pokemon_name.grid(row=1, column=self.column)
-            #pokemon type
-            pokemon_type = Label(frame, text=pokemon.types[0].type.name, bg=CLR)
-            pokemon_type.grid(row=2, column=self.column)
-            frame.grid(row = self.row, column = self.column, pady=10, padx=10)
-            self.column += 1
+        #Getting list of Pokemon and their sprites in a loop
+        pokemon = pb.pokemon(self.pokemon_list[i])
+        sprite = pb.SpriteResource('pokemon', pokemon.id)
+        sprite_url = sprite.url
+        #getting photo to PhotoImage
+        if not os.path.exists('sprites'):
+            os.mkdir('sprites')
 
-            if self.column % 3 == 0:
-                self.column = 0
-                self.row += 1
+        if not os.path.exists(f'sprites/{pokemon.id}.png'):
+            response = requests.get(sprite_url)
+            with open(f"sprites/{pokemon.id}.png", 'wb') as file:
+                file.write(response.content)
 
-            self.images.append(photo)
+        image = Image.open(f'sprites/{pokemon.id}.png')
+        photo = ImageTk.PhotoImage(image)
+        #frame with photo
+        frame = Frame(self.inner_frame, bg=CLR)
+        pokemon_img = Label(frame, image=photo, bg=CLR)
+        pokemon_img.grid(row=0, column=self.column)
+        #Pokedex index
+        pokemon_name = Label(frame, text=f"#{i + 1}", bg=CLR)
+        pokemon_name.grid(row=1, column=self.column)
+        #pokemon name
+        pokemon_name = Label(frame, text=self.pokemon_list[i].capitalize(), bg=CLR)
+        pokemon_name.grid(row=2, column=self.column)
+        #Pokemon type
+        pokemon_type = Label(frame, text=pokemon.types[0].type.name, bg=CLR)
+        pokemon_type.grid(row=3, column=self.column)
+        frame.grid(row = self.row, column = self.column, pady=10, padx=10)
+        self.column += 1
 
+        if self.column % 3 == 0:
+            self.column = 0
+            self.row += 1
 
-            self.inner_frame.update_idletasks()
-            self.canvas.config(scrollregion=self.canvas.bbox("all"))
-
-    def load_pokemon_batch(self, start=1, end=20):
-        if start > end:
-            return
-        self.add_pokemon(start)
-        self.window.after(100, lambda: self.load_pokemon_batch(start + 1, end))
+        self.images.append(photo)
 
 
+        self.inner_frame.update_idletasks()
+        self.canvas.config(scrollregion=self.canvas.bbox("all"))
 
+        self.window.after(40, lambda: self.add_pokemon(i + 1))
--- data.py
+++ data.py
@@ -2,26 +2,21 @@ import requests
 
 
 API_URL = 'https://pokeapi.co/api/v2/pokemon/'
-param = {
-    'offset': 0,
-    'limit': 151
-}
+
 
 class GetData:
     def __init__(self):
         self.pokemon_list = []
-        self.pokemon_sprite_list = []
 
-    def fetch_data(self):
+    def fetch_data(self, param):
         response = requests.get(API_URL, params=param)
         response = response.json()
         list_of_pokemon = response['results']
         list_test = list(list_of_pokemon)
         for i in range(0, len(list_test)):
             self.pokemon_list.append(list_test[i]['name'])
-            self.pokemon_sprite_list.append(list_test[i]['url'])
 
-        print(self.pokemon_list)
-        return self.pokemon_list, self.pokemon_sprite_list
+        #print(self.pokemon_list)
+        return self.pokemon_list
 
 
